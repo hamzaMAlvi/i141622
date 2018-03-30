a00b134328636156d9c380c5359df17e8ebf1b31
hamzaMAlvi <hamzalvi65@gmail.com>
2018-03-30 16:58:46 +0500

    v2

--- src/test/java/st/assignment/LinkedListTest.java
+++ src/test/java/st/assignment/LinkedListTest.java
@@ -8,417 +8,334 @@ public class LinkedListTest {
 
 	@Test
 	public void testSize() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testClear() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testLinkedList() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testLinkedListCollectionOfQextendsE() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testLinkLast() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testLinkBefore() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testUnlink() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetFirst() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetLast() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testRemoveFirst() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testRemoveLast() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAddFirst() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAddLast() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testContainsObject() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAddE() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testRemoveObject() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAddAllCollectionOfQextendsE() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAddAllIntCollectionOfQextendsE() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetInt() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testSetIntE() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAddIntE() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testRemoveInt() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testNode() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testIndexOfObject() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testLastIndexOfObject() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testPeek() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testElement() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testPoll() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testRemove() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testOffer() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testOfferFirst() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testOfferLast() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testPeekFirst() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testPeekLast() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testPollFirst() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testPollLast() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testPush() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testPop() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testRemoveFirstOccurrence() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testRemoveLastOccurrence() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testListIteratorInt() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testDescendingIterator() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testClone() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testToArray() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testToArrayTArray() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testSpliterator() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testIterator() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetInt1() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testSetIntE1() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAddIntE1() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testRemoveInt1() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAddAllIntCollectionOfQextendsE1() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testListIteratorInt1() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAbstractSequentialList() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testHashCode() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testEquals() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAddE1() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAbstractList() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testIndexOfObject1() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testLastIndexOfObject1() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testListIterator() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testSubList() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testRemoveRange() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAbstractCollection() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testIsEmpty() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testContainsObject1() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testToArray1() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testToArrayTArray1() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testRemoveObject1() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testContainsAll() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAddAllCollectionOfQextendsE1() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testRemoveAll() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testRetainAll() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testObject() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetClass() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testClone1() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testNotify() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testNotifyAll() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testWaitLong() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testWaitLongInt() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testWait() {
-		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testFinalize() {
-		fail("Not yet implemented");
 	}
 
 }
